cf81d4235e1e6662bec07e1f41e83669eceeb109
Alina-raa <barmin.rem@gmail.com>
2025-11-28 22:48:11 +0400

    changes have been made to ifat4

--- src/test/java/pagest/ProductsPage.java
+++ src/test/java/pagest/ProductsPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 public class ProductsPage {
     WebDriver driver;
 
-    By pageTitle = By.xpath("//*[data-test='title']");
+    By pageTitle = By.xpath("//*[@data-test='title']");
 
     public ProductsPage(WebDriver driver) {
         this.driver = driver;
